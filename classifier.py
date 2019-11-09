4aef9ce56e8e75a16e69025828cc28141e904067
Lim Kha Shing <kslim5703@gmail.com>
2019-11-10 02:11:17 +0800

    update method of prediction

--- classifier.py
+++ classifier.py
@@ -2,7 +2,6 @@ import numpy as np
 
 from keras.preprocessing import image
 from keras.models import load_model
-from keras.utils.np_utils import to_categorical
 
 IMAGE_WIDTH = 128
 IMAGE_HEIGHT = 128
@@ -21,8 +20,8 @@ model = load_model('./model/model_v3.h5')
 test_image = image.load_img('./dataset_v3/single_prediction/test driving.jpg', target_size = INPUT_SHAPE)
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
-result = model.predict_classes(test_image)
-result = np.argmax(to_categorical(result))
+result = model.predict(test_image)
+result = np.argmax(result[0])
 
 print(result)
 
