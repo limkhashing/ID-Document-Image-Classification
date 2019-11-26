1aa2f62d7060157d795abc043c6f8e66de2624e0
Lim Kha Shing <kslim5703@gmail.com>
2019-11-27 00:04:30 +0800

    update prediction description

--- classifier.py
+++ classifier.py
@@ -7,7 +7,7 @@ IMAGE_WIDTH = 128
 IMAGE_HEIGHT = 128
 IMAGE_CHANNELS = 3
 INPUT_SHAPE = (IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)
-prediction = ''
+predicted_class = ''
 
 # model v2 is binary classifier
 # {'driving_license': 0, 'identity_card': 1}
@@ -19,22 +19,23 @@ model = load_model('./model/model_v3.h5')
 
 test_image = image.load_img('./dataset_v3/single_prediction/test driving.jpg', target_size = INPUT_SHAPE)
 test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = model.predict(test_image)
-result = np.argmax(result[0])
+test_image = np.expand_dims(test_image, axis=0)
+probability = model.predict(test_image)
+prediction = np.argmax(probability[0])
 
-print(result)
+print("Probability: ", probability)
+print("Prediction: ", prediction)
 
 # if result == 0:
 #     prediction = 'driving'
 # elif result == 1:
 #     prediction = 'ic'
 
-if result == 0:
-    prediction = 'driving_license'
-elif result == 1:
-    prediction = 'identity_card'
-elif result == 2:
-    prediction = 'passport'
+if prediction == 0:
+    predicted_class = 'driving_license'
+elif prediction == 1:
+    predicted_class = 'identity_card'
+elif prediction == 2:
+    predicted_class = 'passport'
 
-print(prediction)
+print("Prediction class: ", predicted_class)
